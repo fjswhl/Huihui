fcf5911f5eafdd142eea28b37ced03b2e7cdb5b5
fjswhl <lin@lin.local>
2014-05-04 22:49:48 +0800

    m

---
+++ UIButton+Color.h
@@ -0,0 +1,14 @@
+//
+//  UIButton+Color.h
+//  Huihui
+//
+//  Created by Lin on 14-5-4.
+//  Copyright (c) 2014年 Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (Color)
+- (void)setBackgroundImageWithColor:(UIColor *)color;
+
+@end
---
+++ UIButton+Color.m
@@ -0,0 +1,25 @@
+//
+//  UIButton+Color.m
+//  Huihui
+//
+//  Created by Lin on 14-5-4.
+//  Copyright (c) 2014年 Lin. All rights reserved.
+//
+
+#import "UIButton+Color.h"
+
+@implementation UIButton (Color)
+- (void)setBackgroundImageWithColor:(UIColor *)color{
+    [self setBackgroundImage:[self resizableImageWithColor:color] forState:UIControlStateNormal];
+}
+
+- (UIImage *)resizableImageWithColor:(UIColor *)color{
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(1,1), YES, 0.0);
+    
+    CGContextSetFillColorWithColor(UIGraphicsGetCurrentContext(), color.CGColor);
+    CGContextFillRect(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, 1, 1));
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    
+    return [img resizableImageWithCapInsets:UIEdgeInsetsZero];
+}
+@end
